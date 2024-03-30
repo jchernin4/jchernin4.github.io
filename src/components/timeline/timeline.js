a9549f99c43647e6433bc4ee0bad3ca2a49dc734
jchernin4 <jeremychernin@gmail.com>
2024-03-30 13:44:41 -0400

    Timeline stolen from w3schools

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -1,33 +1,25 @@
 import "./timeline.css"
-import TimelineBox from "../timeline-box";
 
 const Timeline = () => (
-	<div className="grid-container">
-		<TimelineBox title="Blue Demon" date="January 2019" link="https://github.com/jchernin4/BlueDemon" description='My first time using Git and Github. A friend and I worked on an algorithm for a turn-based strategy game called "Fruit Bots", where it competed against other algorithms to collect fruits.'/>
+	<div class="timeline">
+		<TimelineBox c="left" title="Blue Demon" date="January 2019" link="https://github.com/jchernin4/BlueDemon" description='My first time using Git and Github. A friend and I worked on an algorithm for a turn-based strategy game called "Fruit Bots", where it competed against other algorithms to collect fruits.'/>
+		<TimelineBox c="right" title="Poseidon" date="January 2019" link="https://github.com/jchernin4/Poseidon" description="A tool for Discord to automate tasks for administrators, along with some miscellaneous commands. Used by over 100,000 people at it's peak."/>
+	</div> 
+);
 
-		<div className="vl"/>
-		<div className="blank"/>
-		<div className="blank"/>
-		<div className="vl"/>
-
-		<TimelineBox title="Blue Demon" date="January 2019" link="https://github.com/jchernin4/BlueDemon" description='My first time using Git and Github. A friend and I worked on an algorithm for a turn-based strategy game called "Fruit Bots", where it competed against other algorithms to collect fruits.'/>
-		<TimelineBox title="Blue Demon" date="January 2019" link="https://github.com/jchernin4/BlueDemon" description='My first time using Git and Github. A friend and I worked on an algorithm for a turn-based strategy game called "Fruit Bots", where it competed against other algorithms to collect fruits.'/>
-
-		<div className="vl"/>
-		<div className="blank"/>
-		<div className="blank"/>
-		<div className="vl"/>
-
-		<TimelineBox title="Blue Demon" date="January 2019" link="https://github.com/jchernin4/BlueDemon" description='My first time using Git and Github. A friend and I worked on an algorithm for a turn-based strategy game called "Fruit Bots", where it competed against other algorithms to collect fruits.'/>
-		<TimelineBox title="Blue Demon" date="January 2019" link="https://github.com/jchernin4/BlueDemon" description='My first time using Git and Github. A friend and I worked on an algorithm for a turn-based strategy game called "Fruit Bots", where it competed against other algorithms to collect fruits.'/>
-
-		<div className="vl"/>
-		<div className="blank"/>
-		<div className="blank"/>
-		<div className="vl"/>
-
-		<TimelineBox title="Blue Demon" date="January 2019" link="https://github.com/jchernin4/BlueDemon" description='My first time using Git and Github. A friend and I worked on an algorithm for a turn-based strategy game called "Fruit Bots", where it competed against other algorithms to collect fruits.'/>
+const TimelineBox = ({ c, title, date, link, description }) => (
+	<div className={`container ${c}`}>
+		<div className="content">
+			<p>
+				<strong>{title}</strong><br/>
+				<i>{date}</i><br/>
+				<a href={link} target="_blank" rel="noreferrer">{link}</a><br/>
+				{description}
+			</p>
+		</div>
 	</div>
 );
+
+Timeline.TimelineBox = TimelineBox;
 
 export default Timeline;
