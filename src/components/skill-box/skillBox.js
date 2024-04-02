c697d0382edb8078c70f7a3fc6ea51835e280489
jchernin4 <jeremychernin@gmail.com>
2024-04-02 12:40:20 -0400

    GitHub, LinkedIn, and Email links on contact page

--- src/components/skill-box/skillBox.js
+++ src/components/skill-box/skillBox.js
@@ -2,7 +2,7 @@ import "./skillBox.css";
 
 const SkillBox = ({ imageSource, description }) => (
 	<div className="box">
-		<img className="skillimage" src={imageSource}/>
+		<img className="skillimage" src={imageSource} alt=""/>
 		<p className="description">{description}</p>
 	</div>
 );
